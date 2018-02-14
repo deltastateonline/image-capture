d27d21a5160bdf405abed5293d280577c5472a8d
Omo Agbagbara <joomla@deltastateonline.com>
2018-02-14 21:08:49 +1000

    before image component

--- www/js/app.js
+++ www/js/app.js
@@ -15,21 +15,61 @@ Vue.component('page-images', {
 			{photoTitle:'Rear Right', overlayUrl:'/images/overlays/sedan_rear_right.svg', overlayScale: '1.3'}  
 		  ]
 	  }
+  },
+  methods:{
+	fileUploaded(el){
+		
+		console.log(el);
+		
+		/*
+		var eTarget = el.target;
+		if(eTarget.files && eTarget.files[0]){
+			console.log(eTarget.files[0]);
+			var file = eTarget.files[0]
+			var reader = new FileReader();
+			reader.onloadend = function() {
+				 //img.src = reader.result;
+			}
+			reader.readAsDataURL(file);
+			
+		}else{
+			console.log();
+		}*/
+		
+	}
   }
+  
+  
+});
+
+// use slots to add content to 
+Vue.component('anImage',{
+	
+	
 });
 
 
-
-
 Vue.component('page-thanku', {
-  template: '#page-thanku'
+  template: '#page-thanku',
+  data(){
+	  return {
+		  refNumber : "REQ2018-0000001"
+	  }
+  },
+  computed:{
+	  
+	newRefNumber(){
+		var d = new Date();
+		return "RFI-"+d.getFullYear()+""+d.getMonth()+""+d.getDate()+""+d.getMilliseconds();
+	}	
+  }
 });
 Vue.component('page-sorry', {
   template: '#page-sorry'
 });
 
 // Init App
-new Vue({
+var app = new Vue({
   el: '#app',
   // Init Framework7 by passing parameters here
   framework7: {
@@ -68,8 +108,33 @@ new Vue({
 			make:"HOLDEN",
 			model:"CALAIS"
 			
+		},
+		claimRequest:{
+			claimKey:"",
+			
 		}
 		
 		
+	},
+	mounted(){
+		var claimKey = window.location.hash;
+		
+		this.claimRequest.claimKey = claimKey.substr(1);
+		var x = function makeAjax(hashValue){
+			console.log(hashValue)
+		};
+		x(this.claimRequest.claimKey);
+	},
+	methods: {
+		onF7Ready(f7) {
+		  // do some f7 related logic
+		  //f7.dialog.alert('App initialized');
+		  //console.log(f7);
+		  /*
+		  f7.request.get('blog-post.php', { foo:'bar', id:5 }, function (data) {			 
+			  console.log('Load was performed');
+		  });*/
+		  
+		}
 	}
 });
